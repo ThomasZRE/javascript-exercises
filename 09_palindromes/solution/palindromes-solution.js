58b83e6d90b3da7c8a13a0afc86be543c29883b8
Thomas <99052940+ThomasZRE@users.noreply.github.com>
2023-07-31 22:42:28 -0500

    No change

--- 09_palindromes/solution/palindromes-solution.js
+++ 09_palindromes/solution/palindromes-solution.js
@@ -2,5 +2,4 @@ const palindromes = function (string) {
   const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
   return processedString.split("").reverse().join("") == processedString;
 };
-
 module.exports = palindromes;
