fd340f9c3875daf511912a64a79b39f50b481119
Thomas <99052940+ThomasZRE@users.noreply.github.com>
2023-07-31 22:33:16 -0500

    Add solution 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((n, m) => n + m, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+  return arr.reduce((n, m) => n * m, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 0) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let args = Array.from(str.replaceAll(/\W/g, ''));
+    let arr = [];
+    for (let i = 0; i < args.length; i++) {
+        if (args[i] !== " ")
+            arr.unshift(args[i].toLowerCase());
+    }
+    
+    let reversed = Array.from(arr.reverse());
+    arr.reverse();
+    let pairs = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === reversed[i]) {
+            pairs++;
+        }
+    }
 
+    if (pairs === arr.length) {
+        return true;
+    } 
+    return false;
 };
+
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 
 // Do not edit below this line
 module.exports = palindromes;
