59eded84e537f5a2fa339febb4fb62252c9ab40f
Thomas <99052940+ThomasZRE@users.noreply.github.com>
2023-08-02 18:51:09 -0500

    Add sol 11 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(objs) {
+    let titles = [];
+    for (let obj in objs) {
+        titles.push(objs[obj].title);
+    }
+    return titles;
+};          
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort(function(a,b) {
+        if (!a.yearOfDeath) a.yearOfDeath = 2023;
+        if (!b.yearOfDeath) b.yearOfDeath = 2023;
+        const first = a.yearOfDeath - a.yearOfBirth; 
+        const next = b.yearOfDeath - b.yearOfBirth;
+        return next > first ? -1 : 1;
+    });
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
